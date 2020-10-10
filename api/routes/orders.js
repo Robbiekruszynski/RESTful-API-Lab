cd0c55ccdf399b8a377d6415fa0052d6c17dd158
robbie <robbiekruszynski@gmail.com>
2020-10-10 15:27:37 -0700

    order routes all working

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -2,16 +2,31 @@ const { json } = require("express");
 const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: "Order were fetched"
-    });
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Order were fetched",
+  });
 });
 
-router.post ('/', (req, res, next ) => {
-    res.status(201).json({
-        message: "Order weas created"
-    })
-})
+router.post("/", (req, res, next) => {
+  res.status(201).json({
+    message: "Order weas created",
+  });
+});
+//For specific order IDs the /: = dymanic
+router.get("/:orderId", (req, res, next) => {
+  res.status(200).json({
+    message: "Order details",
+    orderId: req.params.orderId,
+  });
+});
+
+//To delete a particular order Id
+router.delete("/:orderId", (req, res, next) => {
+  res.status(200).json({
+    message: "Delete order",
+    orderId: req.params.orderId,
+  });
+});
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
 const productRoutes = require("./api/routes/products");
-//use is a method
-// app.use((req, res, next) => {
-//   res.status(200).json({
-//     message: "working",
-//   });
-// });
+const orderRoutes = require("./api/routes/orders");
 
 app.use("/products", productRoutes);
-//the first argument is the filter /products 
-//so only requests on products will filter to the handle (which is productRoutes) our second argument 
+app.use("/orders", orderRoutes);
+
+//the first argument is the filter /products
+//so only requests on products will filter to the handle (which is productRoutes) our second argument
 //anything going to /products in the URL will now go to the products.js
 
 module.exports = app;
