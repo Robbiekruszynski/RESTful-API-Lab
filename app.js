579acd31a233da8ac915d1aade904f10b3090806
robbie <robbiekruszynski@gmail.com>
2020-10-10 15:34:38 -0700

    add nodemon and morgan packages

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+
+  
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
 
