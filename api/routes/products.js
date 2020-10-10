6bc06a6bfe66663ec9b293818d0380de4ef7cb6e
robbie <robbiekruszynski@gmail.com>
2020-10-10 15:04:45 -0700

    successfully add GET / POST to products

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,4 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 //since we brought in router on line 2 we can start to use this (on line 5) to bring in more routes
-router.
+// get is a method that will handling incoming get requests
+//second argument is the function
+router.get("/", (req, res, next) => {
+  res.setMaxListeners(200).json({
+    message: " Handeling GET requests to /products",
+  });
+});
+
+router.post("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Handeling POST requests to /products",
+  });
+});
+
+module.exports = router;
+ 
--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
-
+const productRoutes = require("./api/routes/products");
 //use is a method
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: "working",
-  });
-});
+// app.use((req, res, next) => {
+//   res.status(200).json({
+//     message: "working",
+//   });
+// });
+
+app.use("/products", productRoutes);
+//the first argument is the filter /products 
+//so only requests on products will filter to the handle (which is productRoutes) our second argument 
+//anything going to /products in the URL will now go to the products.js
 
 module.exports = app;
